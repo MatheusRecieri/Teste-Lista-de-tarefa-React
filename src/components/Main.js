123f3050c7d5f425510c8bf2fb1c25c89b6d2c10
Matheus de Sa Recieri <matheus.recieri@grupofs.local>
2025-10-03 11:27:07 -0300

    FInalizando a seção de react

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,31 @@
 import React, {Component} from "react";
-
 import './Main.css';
-
-//form
-import { FaPlus } from 'react-icons/fa';
-
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import Form from './Form';
+import Tarefas from './Tarefas';
 //classe main que herda Component e vai ser exportada
 export default class Main extends Component {
 
-  // constructor(props) {
-  //   super(props); //chama o construtor da classe que está sendo herdada
-
-  //   this.state = {
-  //     novaTarefa: '',
-  //   };
-    
-  //   //garante que o this referencia o metdo correto
-  //   this.mudaInput = this.mudaInput.bind(this);
-  // }
-  
   // Usando class fields
   state = {
     novaTarefa: '',
     tarefas: [],
     inde: -1
   };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if(tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
 
   //muda o estado (evita o evento de recarga da pagina)
   handleChange = (e) =>  {
@@ -60,9 +60,6 @@ export default class Main extends Component {
         index: -1
       });
     }
-
-   
-
   };
 
   handleDelete = (e, index) => {
@@ -94,26 +91,18 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa}></input>
-          <div className="lista-de-tarefas">
-          </div>
-          <button type="submit">
-            <FaPlus/>
-          </button>
-        </form>
+        <Form
+         handleSubmit={this.handleSubmit}
+         handleChange={this.handleChange}
+         novaTarefa={novaTarefa}
+        />
 
-        <ul className="tarefas">
-            {tarefas.map((tarefa, index) => (
-              <li key={tarefa}>
-                {tarefa}
-                <span>
-                  <FaEdit onClick={ e => this.handleEdit(e, index)} className="edit"/>
-                  <FaWindowClose onClick={ e => this.handleDelete(e, index)} className="delete"/>
-                </span>
-              </li>
-            ))}
-        </ul> 
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
+        
 
       </div>
     );
